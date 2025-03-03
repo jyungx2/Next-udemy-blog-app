a6d4ddeafc3faab4dd22f3d4b46209486497e006
jiyoungnim <cloundyon31@gmail.com>
2025-03-03 15:35:31 +0900

    📝 356. Getting Started with the Home Page

--- pages/index.js
+++ pages/index.js
@@ -1,2 +1,14 @@
-function HomePage() {}
+import { Fragment } from "react";
+
+function HomePage() {
+  return (
+    <Fragment>
+      <Hero />
+      <FeaturedPosts />;
+    </Fragment>
+  );
+}
 export default HomePage;
+
+// 1) Hero => present ourselves
+// 2) Featured Posts
