1ec20d6ab68ca71009865e898f057bc830f5ad96
jiyoungnim <cloundyon31@gmail.com>
2025-03-03 23:44:47 +0900

    📝 375. Sending Data From The Client To the API Route

--- components/contact/contact-form.js
+++ components/contact/contact-form.js
@@ -1,23 +1,65 @@
+import { useState } from "react";
 import classes from "./contact-form.module.css";
 
 function ContactForm() {
+  const [enteredEmail, setEnteredEmail] = useState();
+  const [enteredName, setEnteredName] = useState();
+  const [enteredMessage, setEnteredMessage] = useState();
+
+  async function sendMessageHandler(e) {
+    e.preventDefault();
+
+    // optional: add client-side validation
+
+    // ✅ 여기까지 했을 때, pages/posts/contact.js 파일에서 쓴 console.log(newMessage)에 의해 vscode 터미널에서 내가 입력한 데이터가 보여야 한다.
+    await fetch("/api/contact", {
+      method: "POST",
+      body: JSON.stringify({
+        email: enteredEmail,
+        name: enteredName,
+        message: enteredMessage,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+
   return (
     <section className={classes.contact}>
       <h1>How can I help you?</h1>
-      <form className={classes.form}>
+      <form className={classes.form} onSubmit={sendMessageHandler}>
         <div className={classes.controls}>
           <div className={classes.control}>
             <label htmlFor="email">Your Email</label>
-            <input type="email" id="email" required />
+            <input
+              type="email"
+              id="email"
+              required
+              value={enteredEmail}
+              onChange={(e) => setEnteredEmail(e.target.value)}
+            />
           </div>
           <div className={classes.control}>
             <label htmlFor="name">Your Name</label>
-            <input type="text" id="name" required />
+            <input
+              type="text"
+              id="name"
+              required
+              value={enteredName}
+              onChange={(e) => setEnteredName(e.target.value)}
+            />
           </div>
         </div>
         <div className={classes.control}>
           <label htmlFor="message">Your Message</label>
-          <input type="message" rows="5" required />
+          <textarea
+            type="message"
+            rows="5"
+            required
+            value={enteredMessage}
+            onChange={(e) => setEnteredMessage(e.target.value)}
+          ></textarea>
         </div>
         <div className={classes.actions}>
           <button>Send Message</button>
