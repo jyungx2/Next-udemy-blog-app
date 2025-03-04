2d757110dd7b6ee2d075930c5f171fa3d5a8ef79
jiyoungnim <cloundyon31@gmail.com>
2025-03-04 14:57:54 +0900

    📝 377. Adding UI Feedback with Notifications

--- components/contact/contact-form.js
+++ components/contact/contact-form.js
@@ -1,30 +1,99 @@
 import { useState } from "react";
 import classes from "./contact-form.module.css";
+import Notification from "../ui/notification";
+import { useEffect } from "react";
+
+async function sendContactData(contactDetails) {
+  const res = await fetch("/api/contact", {
+    method: "POST",
+    body: JSON.stringify(
+      //   {
+      //   email: enteredEmail,
+      //   name: enteredName,
+      //   message: enteredMessage,
+      // }
+      contactDetails
+    ),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const data = await res.json();
+
+  if (!res.ok) {
+    throw new Error(data.message || "Something went wrong");
+  }
+}
 
 function ContactForm() {
   const [enteredEmail, setEnteredEmail] = useState();
   const [enteredName, setEnteredName] = useState();
   const [enteredMessage, setEnteredMessage] = useState();
 
+  const [requestStatus, setRequestStatus] = useState();
+  const [requestError, setRequestError] = useState();
+
+  useEffect(() => {
+    if (requestStatus === "success" || requestStatus === "error") {
+      const timer = setTimeout(() => {
+        setRequestStatus(null);
+        setRequestError(null);
+      }, 3000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [requestStatus]);
+
   async function sendMessageHandler(e) {
     e.preventDefault();
 
     // optional: add client-side validation
 
+    setRequestStatus("pending");
+
     // ✅ 여기까지 했을 때, pages/api/contact.js 파일에서 쓴 console.log(newMessage)에 의해 vscode 터미널에서 내가 입력한 데이터가 보여야 한다.
-    await fetch("/api/contact", {
-      method: "POST",
-      body: JSON.stringify({
+
+    try {
+      await sendContactData({
         email: enteredEmail,
         name: enteredName,
         message: enteredMessage,
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+      });
+      setRequestStatus("success");
+      setEnteredEmail("");
+      setEnteredMessage("");
+      setEnteredName("");
+    } catch (error) {
+      setRequestError(error.message);
+      setRequestStatus("error");
+    }
   }
 
+  let notification;
+  if (requestStatus === "pending") {
+    notification = {
+      status: "pending",
+      title: "Sending Message...",
+      message: "Your message is on its way!",
+    };
+  }
+
+  if (requestStatus === "success") {
+    notification = {
+      status: "success",
+      title: "Success!",
+      message: "Message sent successfully!",
+    };
+  }
+
+  if (requestStatus === "error") {
+    notification = {
+      status: "success",
+      title: "Success!",
+      message: requestError,
+    };
+  }
   return (
     <section className={classes.contact}>
       <h1>How can I help you?</h1>
@@ -65,6 +134,13 @@ function ContactForm() {
           <button>Send Message</button>
         </div>
       </form>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
     </section>
   );
 }
